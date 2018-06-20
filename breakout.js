d7ab409a8283f52f2eadd46f9e1a2b66a73b1b9e
Davina Leong <davina.leong.sy@gmail.com>
2018-06-20 21:06:33 +0800

    Added collision to Ball.

--- breakout.js
+++ breakout.js
@@ -1,5 +1,9 @@
 //TODO: Stopped at: Defining a drawing loop
-var colour = "#0095DD";
+var colours = {
+    ball: "#CD3837",
+    brick: "#0095DD",
+    paddle: "#0095DD"
+};
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
@@ -9,18 +13,27 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
+var ballRadius = 10;
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = colour;
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = colours.ball;
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     drawBall();
+
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 
     x += dx;
     y += dy;
