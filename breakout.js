bc63ecf40eb3785a50a0f0e77f1aecda50d5dd6d
Davina Leong <davina.leong.sy@gmail.com>
2018-06-20 21:20:03 +0800

    Put variables into objects and used constants.

--- breakout.js
+++ breakout.js
@@ -1,24 +1,26 @@
 //TODO: Stopped at: Defining a drawing loop
-var colours = {
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const colours = {
     ball: "#CD3837",
     brick: "#0095DD",
     paddle: "#0095DD"
 };
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
 
-var x = canvas.width / 2;
-var y = canvas.height - 30;
-
-var dx = 2;
-var dy = -2;
-
-var ballRadius = 10;
+const ballProps = {
+    x: canvas.width / 2,
+    y: canvas.height - 30,
+    radius: 10,
+    colour: colours.ball,
+    dx: 2,
+    dy: -2
+};
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = colours.ball;
+    ctx.arc(ballProps.x, ballProps.y, ballProps.radius, 0, Math.PI*2);
+    ctx.fillStyle = ballProps.colour;
     ctx.fill();
     ctx.closePath();
 }
@@ -27,16 +29,16 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
 
-    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx = -dx;
+    if(ballProps.x + ballProps.dx > canvas.width - ballProps.radius || ballProps.x + ballProps.dx < ballProps.radius) {
+        ballProps.dx = -ballProps.dx;
     }
 
-    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-        dy = -dy;
+    if(ballProps.y + ballProps.dy > canvas.height - ballProps.radius || ballProps.y + ballProps.dy < ballProps.radius) {
+        ballProps.dy = -ballProps.dy;
     }
 
-    x += dx;
-    y += dy;
+    ballProps.x += ballProps.dx;
+    ballProps.y += ballProps.dy;
 }
 
 setInterval(draw, 10);
