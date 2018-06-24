dd37c9d2a9d04b0ef68f6ecbb41ba31526f12b14
Davina Leong <davina.leong.sy@gmail.com>
2018-06-24 17:03:43 +0800

    Implemented collision detection for bricks

--- breakout.js
+++ breakout.js
@@ -1,169 +1,177 @@
-//TODO: Stopped at: Defining a paddle to hit the ball
-//#region Variable Definition
+'use strict';
+//#region Variables
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const colours = {ball: "#CD3837", brick: "#95DD00", paddle: "#0095DD"};
-const constants = {
-    gameStates: {playing: "PLAYING", gameOver: "GAME OVER"},
-    canvas: {width: canvas.width, height: canvas.height},
-    ballInitial: {
-        position: {x: 240, y: 290},
-        radius: 10,
-        speed: 2
-    },
-    paddleInitial: {
-        position: {x: 202.5, y: 310},
-        dimension: {height: 10, width: 75},
-        speed: 7
-    },
-    bricksInitial: {
-        dimension: {rows: 3, columns: 5, width: 75, height: 20},
-        offset: {padding: 10, top: 30, left: 30},
-        colour: colours.brick
+const colours = { ball: "#CD3837", brick: "#95DD00", paddle: "#0095DD" };
+const gameStates = { ERROR: "ERROR", PLAYING: "PLAYING", LOSE: "LOSE", WIN: "WIN" };
+let currentGameState = gameStates.PLAYING;
+
+const brickStates = { ERROR: "ERROR", NOT_HIT: "NOT_HIT", HIT: "HIT" };
+
+
+//properties
+const keyPressed = { LEFT: false, RIGHT: false };
+const ballProps = {
+    radius: 10,
+    x: 240,
+    y: 290,
+    speed: 2,
+    dx: 2,
+    dy: -2,
+    colour: "#CD3837"
+};
+const paddleProps = {
+    width: 75,
+    height: 10,
+    x: 202.5,
+    y: 310,
+    speed: 7,
+    colour: "#0095DD"
+};
+const brickProps = {
+    rows: 5,
+    columns: 3,
+    width: 75,
+    height: 20,
+    padding: 10,
+    offsetTop: 30,
+    offsetLeft: 30,
+    colour: "#517789"
+};
+
+let brickArr = [];
+for (let c = 0; c < brickProps.columns; ++c) {
+    brickArr[c] = [];
+    for (let r = 0; r < brickProps.rows; ++r) {
+        brickArr[c][r] = { x: 0, y: 0, state: brickStates.NOT_HIT };
     }
-};
-
-const ball = {
-    position: {x: constants.ballInitial.position.x, y: constants.ballInitial.position.y},
-    delta: {x: constants.ballInitial.speed, y: -constants.ballInitial.speed},
-    radius: constants.ballInitial.radius,
-    speed: constants.ballInitial.speed,
-    colour: colours.ball
-};
-
-const paddle = {
-    dimension: {height: constants.paddleInitial.dimension.height, width: constants.paddleInitial.dimension.width},
-    position: {x: constants.paddleInitial.position.x, y: constants.paddleInitial.position.y},
-    speed: constants.paddleInitial.speed,
-    colour: colours.paddle
-};
-
-const buttonPressed = {left: false, right: false};
-
-let bricks = [];
-let currentGameState = constants.gameStates.playing;
+}
 //#endregion
 
-//#region Event Listeners
+//#region Event Handling
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(event) {
-    switch(event.keyCode) {
+    switch (event.keyCode) {
         case 39:
-        buttonPressed.right = true;
-        break;
+            keyPressed.RIGHT = true;
+            break;
 
         case 37:
-        buttonPressed.left = true;
-        break;
+            keyPressed.LEFT = true;
+            break;
 
         default:
-        console.warn(`keyCode ${event.keyCode} not in use.`);
-        break;
+            console.warn(`keyCode ${event.keyCode} not in use.`);
+            break;
     }
 }
 
 function keyUpHandler(event) {
-    switch(event.keyCode) {
+    switch (event.keyCode) {
         case 39:
-        buttonPressed.right = false;
-        break;
+            keyPressed.RIGHT = false;
+            break;
 
         case 37:
-        buttonPressed.left = false;
-        break;
+            keyPressed.LEFT = false;
+            break;
 
         default:
-        console.warn(`keyCode ${event.keyCode} not in use.`);
-        break;
+            console.warn(`keyCode ${event.keyCode} not in use.`);
+            break;
     }
 }
 //#endregion
 
-//#region Functions
+function collisionDetection() {
+    for (let c = 0; c < brickProps.columns; ++c) {
+        for (let r = 0; r < brickProps.rows; ++r) {
+            let thisBrick = brickArr[c][r];
+
+            if (thisBrick.state == brickStates.NOT_HIT) {
+                if (ballProps.x > thisBrick.x && ballProps.x < thisBrick.x + brickProps.width &&
+                    ballProps.y > thisBrick.y && ballProps.y < thisBrick.y + brickProps.height) {
+                    
+                    ballProps.dy = -ballProps.dy;
+                    thisBrick.state = brickStates.HIT;
+                }
+            }
+        }
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, Math.PI*2);
-    ctx.fillStyle = ball.colour;
+    ctx.arc(ballProps.x, ballProps.y, ballProps.radius, 0, Math.PI * 2);
+    ctx.fillStyle = ballProps.colour;
     ctx.fill();
     ctx.closePath();
 }
 
 function drawPaddle() {
     ctx.beginPath();
-    ctx.rect(paddle.position.x, paddle.position.y, paddle.dimension.width, paddle.dimension.height);
-    ctx.fillStyle = paddle.colour;
+    ctx.rect(paddleProps.x, paddleProps.y, paddleProps.width, paddleProps.height);
+    ctx.fillStyle = paddleProps.colour;
     ctx.fill();
     ctx.closePath();
 }
 
 function drawBricks() {
-    const brickProps = constants.bricksInitial;
-    for(let i = 0; i < brickProps.dimension.columns; ++i) {
-        bricks[i] = [];
-        for(let j = 0; j < brickProps.dimension.rows; ++j) {
-            const brick = {
-                index: {row: i, column: j},
-                position: {
-                    x: (i * (brickProps.dimension.width + brickProps.offset.padding)) + brickProps.offset.left,
-                    y: (j * (brickProps.dimension.height + brickProps.offset.padding)) + brickProps.offset.top
-                }
-            };
-            
-            ctx.beginPath();
-            ctx.rect(brick.position.x, brick.position.y, brickProps.dimension.width, brickProps.dimension.height);
-            ctx.fillStyle = brickProps.colour;
-            ctx.fill();
-            ctx.closePath();
+    for (let c = 0; c < brickProps.columns; ++c) {
+        for (let r = 0; r < brickProps.rows; ++r) {
+            if (brickArr[c][r].state == brickStates.NOT_HIT) {
+                brickArr[c][r].x = (r * (brickProps.width + brickProps.padding)) + brickProps.offsetLeft;
+                brickArr[c][r].y = (c * (brickProps.height + brickProps.padding)) + brickProps.offsetTop;
 
-            bricks[i][j] = brick;
+                ctx.beginPath();
+                ctx.rect(brickArr[c][r].x, brickArr[c][r].y, brickProps.width, brickProps.height);
+                ctx.fillStyle = brickProps.colour;
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
 
 function draw() {
-    //#region Draw Graphics
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks();
     drawBall();
     drawPaddle();
-    //#endregion
+    collisionDetection();
 
-    //#region Ball
-    if(ball.position.x + ball.delta.x > canvas.width - ball.radius || ball.position.x + ball.delta.x < ball.radius) {
-        ball.delta.x = -ball.delta.x;
+    if (ballProps.x + ballProps.dx > canvas.width - ballProps.radius ||
+        ballProps.x + ballProps.dx < ballProps.radius) {
+        ballProps.dx = -ballProps.dx;
     }
-
-    if(ball.position.y + ball.delta.y < ball.radius) {
-        ball.delta.y = -ball.delta.y;
+    if (ballProps.y + ballProps.dy < ballProps.radius) {
+        ballProps.dy = -ballProps.dy;
     }
-    else if(ball.position.y + ball.delta.y > canvas.height + ball.radius + 10) {
-        if(ball.position.x > paddle.position.x && ball.position.x < paddle.position.x + paddle.dimension.width) {
-            ball.delta.y = -ball.delta.y;
+    else if (ballProps.y + ballProps.dy > canvas.height + ballProps.radius) {
+        if (ballProps.x > paddleProps.x && ballProps.x < paddleProps.x + paddleProps.width) {
+            ballProps.dy = -ballProps.dy;
         }
         else {
-            ball.delta = {x: 0, y: 0};
-            currentGameState = constants.gameStates.gameOver;
+            ballProps.dx = 0;
+            ballProps.dy = 0;
+            currentGameState = gameStates.LOSE;
             alert("GAME OVER");
             document.location.reload();
         }
     }
 
-    ball.position.x += ball.delta.x;
-    ball.position.y += ball.delta.y;
-    //#endregion
-
-
-    //#region Paddle
-    if(buttonPressed.right && paddle.position.x < canvas.width - paddle.dimension.width) {
-        paddle.position.x += paddle.speed;
-    } else if(buttonPressed.left && paddle.position.x > 0) {
-        paddle.position.x -= paddle.speed;
+    if (keyPressed.RIGHT && paddleProps.x < canvas.width - paddleProps.width) {
+        paddleProps.x += 7;
     }
-    //#endregion
+    else if (keyPressed.LEFT && paddleProps.x > 0) {
+        paddleProps.x -= 7;
+    }
+
+    ballProps.x += ballProps.dx;
+    ballProps.y += ballProps.dy;
 }
 
-if(currentGameState == constants.gameStates.playing) setInterval(draw, 10);
-//#endregion
+if(currentGameState == gameStates.PLAYING) setInterval(draw, 10);
